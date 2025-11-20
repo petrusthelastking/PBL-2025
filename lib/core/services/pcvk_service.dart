f21e42b32bedd0aba5699ace65397b5777e12696
Raruu Kagami <108914099+Raruu@users.noreply.github.com>
2025-11-20 19:20:55 +0700

    feat: pcvk api test

--- lib/core/services/pcvk_service.dart
+++ lib/core/services/pcvk_service.dart
@@ -2,12 +2,14 @@ import 'dart:convert';
 import 'dart:io' show File;
 
 import 'package:http/http.dart' as http;
+import 'package:jawara/core/models/PCVK/batch_predict_response.dart';
 import 'package:jawara/core/models/PCVK/health_response.dart';
 import 'package:jawara/core/models/PCVK/models_response.dart';
 import 'package:jawara/core/models/PCVK/predict_response.dart';
 
 class PcvkService {
-  final String _apiUrl = 'http://127.0.0.1:8000/api/';
+  final String _apiUrl =
+      'https://pcvk-containerapp.lemonisland-43c085da.southeastasia.azurecontainerapps.io/';
   late final http.Client _client;
 
   PcvkService({http.Client? httpClient}) {
@@ -28,7 +30,7 @@ class PcvkService {
   Future<List<String>> getClasses() async {
     final response = await _client.get(_buildEndpoint('classes'));
     if (response.statusCode == 200) {
-      return List<String>.from(json.decode(response.body));
+      return List<String>.from(json.decode(response.body)['classes']);
     } else {
       throw Exception('Failed to fetch classes - ${response.statusCode}');
     }
